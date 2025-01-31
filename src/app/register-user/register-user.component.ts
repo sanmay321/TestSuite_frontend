cc6343008da3ea9a44981309dbfd2d96de9ff7c1
Blen <blenassefa59@yahoo.com>
2025-01-31 19:29:48 +0300

    style: update Sign Up modal styling

--- src/app/register-user/register-user.component.ts
+++ src/app/register-user/register-user.component.ts
@@ -1,10 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder,FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import {MatDialogModule} from '@angular/material/dialog';
 import { MatDialogRef } from '@angular/material/dialog';
 import {MatTabsModule} from '@angular/material/tabs';
 import { SigninComponent } from "../signin/signin.component";
+
+interface SignupForm {
+  firstName: FormControl<string>;
+  lastName: FormControl<string>;
+  email: FormControl<string>;
+  phone: FormControl<string | null>;
+  company: FormControl<string>;
+  country: FormControl<string>;
+  users: FormControl<string>;
+  webAddress: FormControl<string>;
+  terms: FormControl<boolean>;
+  newsletter: FormControl<boolean>;
+}
 
 @Component({
   selector: 'app-register-user',
@@ -14,7 +27,7 @@ import { SigninComponent } from "../signin/signin.component";
   imports: [ReactiveFormsModule, CommonModule, MatDialogModule, SigninComponent, MatTabsModule]
 })
 export class RegisterUserComponent {
-  signupForm: FormGroup;
+  signupForm: FormGroup<SignupForm>;
   signupFlag: boolean = false;
 
   constructor(private fb: FormBuilder, public dialogRef: MatDialogRef<RegisterUserComponent>) {
@@ -28,20 +41,30 @@ export class RegisterUserComponent {
     //   numberOfUsers: ['', Validators.required],
     //   webAddress: ['', Validators.required]
     // });
-
-    this.signupForm = this.fb.group({
-      username: ['', Validators.required],
-      password: ['', Validators.required],
-      confirmPassword: ['', Validators.required],
-    }, { validator: this.passwordMatchValidator });
+    
+    this.signupForm = new FormGroup<SignupForm>({
+      firstName: new FormControl('', { nonNullable: true, validators: Validators.required }),
+      lastName: new FormControl('', { nonNullable: true, validators: Validators.required }),
+      email: new FormControl('', { nonNullable: true, validators: [Validators.required, Validators.email] }),
+      phone: new FormControl<string | null>(null),
+      company: new FormControl('', { nonNullable: true, validators: Validators.required }),
+      country: new FormControl('', { nonNullable: true, validators: Validators.required }),
+      users: new FormControl('', { nonNullable: true, validators: Validators.required }),
+      webAddress: new FormControl('', { nonNullable: true, validators: Validators.required }),
+      terms: new FormControl(false, { nonNullable: true, validators: Validators.requiredTrue }),
+      newsletter: new FormControl(false, { nonNullable: true })
+    });
+    
+  
   }
 
-  passwordMatchValidator(form: FormGroup) {
-    const password = form.get('password')?.value;
-    const confirmPassword = form.get('confirmPassword')?.value;
+  // passwordMatchValidator(form: FormGroup) {
+  //   const password = form.get('password')?.value;
+  //   const confirmPassword = form.get('confirmPassword')?.value;
 
-    return password === confirmPassword ? null : { mismatch: true };
-  }
+  //   // return password === confirmPassword ? null : { mismatch: true };
+  //   return true
+  // }
 
   onSubmit() {
     if (this.signupForm.valid) {
